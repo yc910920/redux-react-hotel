499d512f65389331b39fbdfbe8c0d337cdba75e7
yc910920 <missing.yc@gmail.com>
2016-08-13 23:58:02 +0800

    bug fixed
    bug fixed

--- src/middleware/promiseMiddleware.js
+++ src/middleware/promiseMiddleware.js
@@ -2,7 +2,7 @@
 
 export default function promiseMiddleware() {
   return next => action => {
-    const { promise, type, ...rest } = action;
+    const { promise, type } = action;
 
     if (!promise) return next(action);
 
@@ -11,16 +11,16 @@ export default function promiseMiddleware() {
     const REQUEST = type + '_REQUEST';
     const FAILURE = type + '_FAILURE';
 
-    next({ ...rest, type: REQUEST });
+    next({  type: REQUEST });
 
     return promise
       .then(res => {
-        next({ ...rest, res, type: SUCCESS });
+        next({  res, type: SUCCESS });
 
         return true;
       })
       .catch(error => {
-        next({ ...rest, error, type: FAILURE });
+        next({ error, type: FAILURE });
         console.log(error);
 
         return false;
